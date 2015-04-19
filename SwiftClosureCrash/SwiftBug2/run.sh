30c2491a46ff0bce07146ef5f7bcca2e3e2dd644
Yang Liu <yaliu@linkedin.com>
2015-04-18 22:52:51 -0700

    added comments

--- SwiftClosureCrash/SwiftBug2/run.sh
+++ SwiftClosureCrash/SwiftBug2/run.sh
@@ -1,1 +1,2 @@
-swift ConcurrentOperation.swift FetchAndStoreOperation.swift ClosureFetchAndStoreOperation.swift main.swift
+#This won't be able to reproduce the issue. Need to run with the project file
+xcrun swift ConcurrentOperation.swift FetchAndStoreOperation.swift ClosureFetchAndStoreOperation.swift main.swift
