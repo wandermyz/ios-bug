23b235d1c4c40fa9488eebc0a19af98edb536a97
Wander <lywander@gmail.com>
2015-04-18 22:39:46 -0700

    fix

--- SwiftFlatmapRuntimeIssue/magic.swift
+++ SwiftFlatmapRuntimeIssue/magic.swift
@@ -3,7 +3,7 @@ func magic<T, U>(t:T, f:(T)->U?) -> [U] {
   }
 println(magic(10, { (v:Int) -> Float in Float(v) } ))
 
-// Expected Output: 10.0
-// Actual Output: 0.0
+// Expected: Compilation error
+// Actual Output: 0.0 or other random numbers
 
 // Seems fixed in Swift 1.2
