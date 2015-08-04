fa17396984d625f99f7434a41656c003ae9957aa
Yang Liu <yaliu@linkedin.com>
2015-08-03 18:35:01 -0700

    won't crash

---
+++ GenericSubclass/wontCrash.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class A<T:AnyObject> { //won't crash
+  func foo() -> T {
+    fatalError("Abstract")
+  }
+
+  func bar() -> T {
+    return foo()
+  }
+}
+
+class B<T>: A<NSNumber> {
+  override func foo() -> NSNumber {
+    return NSNumber(integer: 1)
+  }
+}
+
+let b = B<AnyObject>()
+println(b.bar()) 
